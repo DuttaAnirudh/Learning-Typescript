185a8bb67007906da84318f0b6db1701bf95bf72
DuttaAnirudh <worktodutta@gmail.com>
2024-08-05 23:08:42 +0530

    core types in typescript; type assignment and type inference in typescript

--- 02 - Project 1/app.ts
+++ 02 - Project 1/app.ts
@@ -1,1 +1,15 @@
-console.log('Your code goes here...');
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  if (showResult) {
+    console.log(`${phrase} ${n1 + n2}`);
+  }
+
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const phraseResult = "Result is: ";
+
+const result = add(number1, number2, printResult, phraseResult);
+console.log(result);
